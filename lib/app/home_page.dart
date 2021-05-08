f068732c69b8c1ff192f03aca25d9e8d673934eb
tagirahmad <tagirbpl@gmail.com>
2021-05-08 21:25:01 +0300

    Add provider; implement provider

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/common_widgets/show_alert_dialog.dart';
-import 'package:time_tracker_flutter_course/services/auth_provider.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class HomePage extends StatelessWidget {
   Future<void> _singOut(BuildContext context) async {
     try {
-      final auth = AuthProvider.of(context);
+      final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signOut();
     } catch (e) {
       print(e.toString());
